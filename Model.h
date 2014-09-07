628a884b1073f4483e59a40c48e64cf54adaf2c3
DB <dbruenner@Davids-MacBook-Pro.local>
2014-09-07 20:50:55 +0200

    2.2.2

---
+++ Model.h
@@ -0,0 +1,15 @@
+//
+//  Model.h
+//  Kapitel2
+//
+//  Created by David Brünner on 07.09.14.
+//  Copyright (c) 2014 David Brünner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+@property(copy) NSString *status;
+@property(strong) NSDate *creation;
+@property(copy) NSString *name;
+@end
