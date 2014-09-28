fd8b71ef6bfcff2f9bf8513bb4dc17589667da20
DB <dbruenner@Davids-MacBook-Pro.local>
2014-09-28 17:50:06 +0200

    2.2.2 Modellbau

---
+++ Kapitel2/Kapitel2/Model.m
@@ -0,0 +1,20 @@
+//
+//  Model.m
+//  Kapitel2
+//
+//  Created by David Brünner on 28.09.14.
+//  Copyright (c) 2014 David Brünner. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model {
+    @private
+    NSMutableArray *objects;
+}
+
+@synthesize status;
+@synthesize name;
+@synthesize creation;
+
+@end
