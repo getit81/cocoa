e8271269a306381f90ce4c607f982d20cc7f9b1d
DB <dbruenner@Davids-MacBook-Pro.local>
2014-10-06 14:03:51 +0200

    3.1 Eigene Viewklassen in Cocoa Touch

---
+++ Kapitel3/AlarmClock/AlarmClock/ClockView.h
@@ -0,0 +1,13 @@
+//
+//  ClockView.h
+//  AlarmClock
+//
+//  Created by David Brünner on 06.10.14.
+//  Copyright (c) 2014 David Brünner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClockView : UIView
+
+@end
---
+++ Kapitel3/AlarmClock/AlarmClock/ClockView.m
@@ -0,0 +1,27 @@
+//
+//  ClockView.m
+//  AlarmClock
+//
+//  Created by David Brünner on 06.10.14.
+//  Copyright (c) 2014 David Brünner. All rights reserved.
+//
+
+#import "ClockView.h"
+
+@implementation ClockView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+
+@end
